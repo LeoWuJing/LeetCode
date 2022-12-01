d340c15b260840340cfed5fc99fa6ead50b9cd68
Administrator <1242334451@qq.com>
2022-12-01 22:10:12 +0800

    commit

--- questions/src/tree/TreePaths.java
+++ questions/src/tree/TreePaths.java
@@ -8,8 +8,6 @@ import java.util.List;
 如果当前节点是叶子节点，则在当前路径末尾添加该节点后我们就得到了一条从根节点到叶子节点的路径，将该路径加入到答案即可
 */
 public class TreePaths {
-    List<String> res = new ArrayList<>();
-
     public List<String> binaryTreePaths(TreeNode root) {
         List<String> res = new ArrayList<>();
         if (root == null) {
